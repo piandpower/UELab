e4d50f17df13a2dd4f29c9bf535788946c405807
Qi <wwqo@163.com>
2017-11-20 00:06:58 +0800

    add file

---
+++ UELab/Source/UELab/UELab.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UELab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UELab, "UELab" );
---
+++ UELab/Source/UELab/UELabGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UELabGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UELAB_API AUELabGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
